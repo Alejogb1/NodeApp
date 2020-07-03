20114f5bd19c669a3df96812665b3f3f46c32429
Alejo <alejogarb25@gmail.com>
2020-07-03 20:35:25 -0300

    Fixed RefenceError issue

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const exphbs = require("express-handlebars")
 const connectDB = require("./config/db")
 const connectMongoDB = require("./config/db")
 const morgan = require("morgan")
+const app = express()
 
 // Load config
 dotenv.config({path: "./config/config.env"})
 
 connectMongoDB()
+
 // Logging
-if (process.env.NODE_ENV == "development"){
+if (process.env.NODE_ENV === "development"){
     app.use(morgan("dev"))
 } 
 // Handlebars
@@ -18,8 +20,9 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
   
 // Routes
+
 app.use("/", require("./routes/index"))
-const app = express()
+
 
 const PORT = process.env.PORT || 5000
 
