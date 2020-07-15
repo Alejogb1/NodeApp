64d2a06ef659654f2266cf4da9569f2fa7afa5d0
Alejo <alejogarb25@gmail.com>
2020-07-15 11:32:48 -0300

    Passport sessions

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const connectDB = require("./config/db")
 const connectMongoDB = require("./config/db")
 const morgan = require("morgan")
 const app = express()
+const passport = require("passport")
+const session = ("express-session")
 
 // Load config
 dotenv.config({path: "./config/config.env"})
+
+// Passport config
+
+require("./config/passport")(passport) // Second parentesis is an argument
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 connectMongoDB()
 
@@ -22,6 +32,13 @@ app.engine('.hbs', exphbs({defaultLayout: 'main', extname: ".hbs"}));
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", ".hbs");
 
+// Sessions 
+app.use(session ({
+    secret: "keyboard cat",
+    resave: false, // We don't want to save  a session if nothinh is modified
+    saveUninitialized: true, // Don't create a session until smt is stored
+   /* cookie: { secure: true }*/ // This won't work without https
+}))
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
 // Routes
