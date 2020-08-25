e2d85a740f5f48192771bb8525b91422aa4f0b70
Alejo <alejogarb25@gmail.com>
2020-08-25 18:04:53 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function (passport){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/dashboard"
+        callbackURL: "http://localhost:3000/dashboard"
       },
       async (accessToken, refreshToken, profile, done) => {
           const newUser = { // By creating a "newUser", MD creates an unique ID for it.
