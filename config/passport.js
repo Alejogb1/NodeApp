1e2bd819171ee94417e5c5abb768962d2397a176
Alejo <alejogarb25@gmail.com>
2020-08-25 18:08:12 -0300

    var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User")
 
